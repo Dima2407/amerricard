d1e4f94b7a858fa35cfb86e7f4ec348cc75d252c
Nikolay Dymura <nikolay.dymura@gmail.com>
2017-09-13 13:17:11 +0300

    Fixed incorrect user on notification.

--- app/src/main/java/com/devtonix/amerricard/services/HolidaysNotificationService.java
+++ app/src/main/java/com/devtonix/amerricard/services/HolidaysNotificationService.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
@@ -64,54 +65,11 @@ public class HolidaysNotificationService extends Service {
         Date today = new Date();
         SimpleDateFormat dateFormat = RegexDateUtils.GODLIKE_APPLICATION_DATE_FORMAT;
 
-        final List<EventItem> events = eventRepository.getEventFromStorage();
-        final List<EventItem> eventsForDisplay = new ArrayList<>();
-        for (EventItem item : events) {
-            try {
-
-                Date bDay = dateFormat.parse(item.getFormattedDate());
-                if (TimeUtils.isSameDay(bDay, new Date())) {
-                    eventsForDisplay.add(item);
-                    Log.d(TAG, "onCreate: eventForDisplay.add(" + item.getName() + ")");
-                }
-            } catch (ParseException e) {
-                Log.e(TAG, "onCreate: ", e);
-            }
-        }
+        final List<EventItem> eventsForDisplay = getTodayEvents(today, dateFormat);
 
         final List<Celebrity> celebrities = celebrityRepository.getCelebritiesFromStorage();
-        final List<Celebrity> celebritiesForDisplay = new ArrayList<>();
-        for (Celebrity c : celebrities) {
-            try {
-                Log.d(TAG, "onCreate: celebrity = " + c.getName());
-
-                Date bDay = dateFormat.parse(c.getFormattedDate());
-
-                if (TimeUtils.isSameDay(bDay, today)) {
-                    celebritiesForDisplay.add(c);
-                    Log.d(TAG, "onCreate: celebritiesForDisplay.add(" + c.getName() + ") ");
-                }
-            } catch (ParseException e) {
-                Log.e(TAG, "onCreate: ", e);
-            }
-        }
-
-        final List<Contact> contacts = contactRepository.getContactsFromStorage();
-        final List<Contact> contactsForDisplay = new ArrayList<>();
-        for (Contact c : contacts) {
-            try {
-                Log.d(TAG, "onCreate: contact = " + c.getName());
-                Date bDay = dateFormat.parse(c.getBirthday());
-
-
-                if (TimeUtils.isSameDay(bDay, today) && !sharedHelper.getContactsForHide().contains(c)) {
-                    contactsForDisplay.add(c);
-                    Log.d(TAG, "onCreate: contactsForDisplay.add(" + c.getName() + ") ");
-                }
-            } catch (ParseException e) {
-                Log.e(TAG, "onCreate: ", e);
-            }
-        }
+        final List<Celebrity> celebritiesForDisplay = getTodayCelebrities(today, dateFormat);
+        final List<Contact> contactsForDisplay = getTodayContacts(today, dateFormat);
 
         if (eventsForDisplay.size() > 0 || contactsForDisplay.size() > 0 || celebritiesForDisplay.size() > 0) {
 
@@ -137,7 +95,7 @@ public class HolidaysNotificationService extends Service {
             StringBuilder forDisplay = new StringBuilder();
 
             if (contactsForDisplay.size() != 0) {
-                forDisplay.append(contacts.get(0).getName()).append(" happy birthday!");
+                forDisplay.append(contactsForDisplay.get(0).getName()).append(" happy birthday!");
             } else if (eventsForDisplay.size() != 0) {
                 forDisplay.append(LanguageUtils.convertLang(eventsForDisplay.get(0).getName(), sharedHelper.getLanguage())).append(" is celebrated today");
             } else if (celebritiesForDisplay.size() != 0 && sharedHelper.getCelebritiesInSettings()) {
@@ -160,5 +118,66 @@ public class HolidaysNotificationService extends Service {
         }
 
         stopSelf();
+    }
+
+    @NonNull
+    private List<EventItem> getTodayEvents(Date today, SimpleDateFormat dateFormat) {
+        final List<EventItem> events = eventRepository.getEventFromStorage();
+        final List<EventItem> eventsForDisplay = new ArrayList<>();
+        for (EventItem item : events) {
+            try {
+
+                Date bDay = dateFormat.parse(item.getFormattedDate());
+                if (TimeUtils.isSameDay(bDay, today)) {
+                    eventsForDisplay.add(item);
+                    Log.d(TAG, "onCreate: eventForDisplay.add(" + item.getName() + ")");
+                }
+            } catch (ParseException e) {
+                Log.e(TAG, "getTodayEvents: ", e );
+            }
+        }
+        return eventsForDisplay;
+    }
+
+    @NonNull
+    private List<Celebrity> getTodayCelebrities(Date today, SimpleDateFormat dateFormat){
+        final List<Celebrity> celebrities = celebrityRepository.getCelebritiesFromStorage();
+        final List<Celebrity> celebritiesForDisplay = new ArrayList<>();
+        for (Celebrity c : celebrities) {
+            try {
+                Log.d(TAG, "onCreate: celebrity = " + c.getName());
+
+                Date bDay = dateFormat.parse(c.getFormattedDate());
+
+                if (TimeUtils.isSameDay(bDay, today)) {
+                    celebritiesForDisplay.add(c);
+                    Log.d(TAG, "onCreate: celebritiesForDisplay.add(" + c.getName() + ") ");
+                }
+            } catch (ParseException e) {
+                Log.e(TAG, "getTodayCelebrities: ", e);
+            }
+        }
+        return celebritiesForDisplay;
+    }
+
+    @NonNull
+    private List<Contact> getTodayContacts(Date today, SimpleDateFormat dateFormat){
+        final List<Contact> contacts = contactRepository.getContactsFromStorage();
+        final List<Contact> contactsForDisplay = new ArrayList<>();
+        for (Contact c : contacts) {
+            try {
+                Log.d(TAG, "onCreate: contact = " + c.getName());
+                Date bDay = dateFormat.parse(c.getBirthday());
+
+
+                if (TimeUtils.isSameDay(bDay, today) && !sharedHelper.getContactsForHide().contains(c)) {
+                    contactsForDisplay.add(c);
+                    Log.d(TAG, "onCreate: contactsForDisplay.add(" + c.getName() + ") ");
+                }
+            } catch (ParseException e) {
+                Log.e(TAG, "getTodayContacts: ", e);
+            }
+        }
+        return contactsForDisplay;
     }
 }
